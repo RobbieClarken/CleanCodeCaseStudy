70bd43dde37ecc67071992374aaccd06b4c4d5af
RobbieClarken <robbie.clarken@gmail.com>
2017-04-02 20:29:34 +1000

    Update junit and hierarchicalcontextrunner

--- project.clj
+++ project.clj
@@ -1,13 +1,10 @@
 (defproject casestudy "cleancoderscasestudy"
   :description "Case Study"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [junit/junit "4.11"]
+                 [junit/junit "4.12"]
                  [org.fitnesse/fitnesse "20140630"]
-                 [de.bechte.junit/junit-hierarchicalcontextrunner "4.11.3"]]
+                 [de.bechte.junit/junit-hierarchicalcontextrunner "4.12.1"]]
 
   :repositories [["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"}]]
-
   :aliases {"fitnesse" ["run" "-m" "fitnesseMain.FitNesseMain" "-p" "8080"]}
   )
-
-
